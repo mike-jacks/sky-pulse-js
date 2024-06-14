c7d72eec80ef2c8b871e46bddbab120af3978f32
Mike Jacks <mjacks4816@xmail.dixietech.edu>
2024-06-13 23:13:26 -0600

    update next.config.mjs

--- sky-pulse/next.config.mjs
+++ sky-pulse/next.config.mjs
@@ -16,6 +16,9 @@ const nextConfig = {
   output: "export",
   basePath: isProd ? "/sky-pulse-js" : "",
   assetPrefix: isProd ? "/sky-pulse-js" : "",
+  env: {
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+  },
 };
 
 export default nextConfig;
